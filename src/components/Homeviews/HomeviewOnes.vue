7eecd49fc2c61c891310e76b3259390cf373ed41
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-12-16 11:28:01 +0500

    update

--- src/components/Homeviews/HomeviewOnes.vue
+++ src/components/Homeviews/HomeviewOnes.vue
@@ -47,7 +47,8 @@
         </div>
       </div>
     </div>
-    </template>
+
+  </template>
     
     <script setup>
     
@@ -55,91 +56,92 @@
     
     <style lang="scss" scoped>
     @import url("https://fonts.googleapis.com/css?family=Muli:400,700&display=swap");
-    
 
-    
-    h1 {
-      text-align: center;
-      margin: 1.5em;
-    }
-    
-    
-    
-    .container {
-      width: 100%;
-      position: relative;
-    }
-    
-    .items {
-      display: flex;
-      align-items: center;
-      width: fit-content;
-      width: max-content;
-      animation: carouselAnim 60s infinite alternate linear;
-      overflow-x: auto;
+body {
+  margin: 0;
+  height: calc(100vh - 8em);
+  font-family: "Muli", sans-serif;
+}
 
+h1 {
+  text-align: center;
+  margin: 1.5em;
+}
+
+.container {
+  width: 100%;
+  position: relative;
+}
+
+.items {
+  display: flex;
+  gap: 44px;
+  align-items: center;
+  width: fit-content;
+  width: max-content;
+  animation: carouselAnim 20s infinite alternate linear;
+  overflow-x: auto;
+}
+
+.entry {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  width: 200px;
+  margin: 1em;
+  padding: 0.5em;
+}
+
+@media only screen and (max-width: 768px) {
+  .items {
+    animation: carouselAnim 20s infinite alternate linear;
+    overflow: hidden;
+  }
+
+  @keyframes carouselAnim {
+    from {
+      transform: translate(0, 0);
     }
-    
-    .entry {
-      display: flex;  
-      align-items: center;
-      flex-direction: column;
-      position: relative;
-      width: 200px;
-      margin: 4em;
-      padding: 0.5em;  
+    to {
+      transform: translate(calc(-100% + (2 * 300px)));
     }
-    
-    @media only screen and (max-width: 768px) {
-      .items {
-        animation: carouselAnim 60s infinite alternate linear;
-        overflow-x: auto;
-      }
-      
-      @keyframes carouselAnim {
-        from {
-          transform: translate(-100%);
-        }
-        to {
-          transform: translate(0);
-        }
-      }
-    }
-    
-    .entry p {
-      text-align: center;
-      padding: 1em;
-    }
-    
-    .name {
-      font-weight: bold;
-    }
-    
-    @keyframes carouselAnim {
-      from {
-        transform: translate(-100%);
-      }
-      to {
-        transform: translate(calc(0));
-      }
-    }
-    
-    .container .faders {
-      width: 100%;
-      position: absolute;
-      height: 100%;
-    }
-    
-    .faders .right,
-    .faders .left {
-      width: 15%;
-      height: 100%;
-      position: absolute;
-      z-index: 200;
-    }
-    
-    .right {
-      right: 0;
-    }
-    
+  }
+}
+
+.entry p {
+  text-align: center;
+  padding: 1em;
+}
+
+.name {
+  font-weight: bold;
+}
+
+@keyframes carouselAnim {
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(calc(-100% + (5 * 300px)));
+  }
+}
+
+.container .faders {
+  width: 100%;
+  position: absolute;
+  height: 100%;
+}
+
+.faders .right,
+.faders .left {
+  width: 15%;
+  height: 100%;
+  position: absolute;
+  z-index: 200;
+}
+
+.right {
+  right: 0;
+}
     </style>
