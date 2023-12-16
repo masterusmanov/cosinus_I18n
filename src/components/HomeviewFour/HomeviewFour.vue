23f2fd5c8a3bdb9946efa929322186b0a6a8ee2d
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-12-16 10:03:11 +0500

    update

--- src/components/HomeviewFour/HomeviewFour.vue
+++ src/components/HomeviewFour/HomeviewFour.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="text-white m-5 mt-[68px] md:mt-[150px] lg:ml-[150px]">
+    <div class="text-white m-5 mt-[128px] md:mt-[150px] xl:ml-[150px] font-Satoshi">
         <p id="yangi" class="text-[16px] font-[700] md:text-[21px] md:ml-[104px] lg:ml-[128px]">{{$t('translation.homeviewfour.one')}}</p>
-        <h2 class="text-[27px] md:text-[36px] lg:text-[61px] font-[900] font-sans mt-[18px] md:ml-[104px] lg:ml-[128px] lg:w-[60%]">{{$t('translation.homeviewfour.two')}}</h2>
-        <p class="text-[16px] md:text-[21px] mt-[32px] md:ml-[104px] lg:ml-[128px] lg:w-[50%]">{{$t('translation.homeviewfour.three')}}</p>
+        <h2 class="text-[27px] md:text-[36px] lg:text-[61px] font-[900] mt-[18px] md:ml-[104px] lg:ml-[128px] 2xl:w-[60%]">{{$t('translation.homeviewfour.two')}}</h2>
+        <p class="text-[16px] md:text-[21px] mt-[32px] md:ml-[104px] lg:ml-[128px] lg:w-[70%] xl:w-[50%]">{{$t('translation.homeviewfour.three')}}</p>
     </div>
     <img src="../../assets/images/speceffect/polborder.png" alt="" class="hidden md:flex absolute w-[150px] h-[135px] mb-[20px] ml-[500px] lg:ml-[1000px] lg:mt-[15px] 2xl:ml-[1250px] 2xl:mt-[15px]">
     <img src="../../assets//images/speceffect/Shape.png" alt="" class="hidden lg:flex absolute lg:ml-[900px] lg:mt-[850px] 2xl:ml-[1175px]">
@@ -21,6 +21,7 @@
 
 <style lang="scss" scoped>
     #yangi{
+        width: 220px;
         background: var(--Gradient, linear-gradient(269deg, rgba(28, 31, 229, 0.68) -13.91%, rgba(54, 197, 158, 0.90) 107.18%));
         background-clip: text;
         -webkit-background-clip: text;
--- src/components/HomeviewThree/HomeviewThree.vue
+++ src/components/HomeviewThree/HomeviewThree.vue
@@ -3,7 +3,7 @@
         <p id="yangi" class="text-[16px] font-[700] md:text-[21px] md:ml-[104px] lg:ml-[128px]">{{$t('translation.homeviewthree.one')}}</p>        
         <img src="../../assets/images/speceffect/rounded.png" alt="" class="hidden md:flex absolute lg:w-[54px] md:h-[54px] md:mt-[300px] md:ml-[630px] lg:mt-[400px] lg:ml-[800px]">
         <h2 class="text-[27px] md:text-[36px] lg:text-[61px] font-[900] font-Satoshi mt-[18px] md:ml-[104px] lg:ml-[128px] 2xl:w-[60%]">{{$t('translation.homeviewthree.two')}}</h2>
-        <p class="text-[16px] md:text-[21px] mt-[32px] md:ml-[104px] lg:ml-[128px] lg:w-[50%]">{{$t('translation.homeviewthree.three')}}</p>
+        <p class="text-[16px] md:text-[21px] mt-[32px] md:ml-[104px] lg:ml-[128px] lg:w-[70%] xl:w-[50%]">{{$t('translation.homeviewthree.three')}}</p>
     </div>
     <img src="../../assets/images/speceffect/polborder3.png" alt="" class="hidden lg:flex w-[150px] h-[135px] absolute mt-[320px]">
     <div class="mt-[64px] md:mt-[96px] lg:mt-[128px]">
